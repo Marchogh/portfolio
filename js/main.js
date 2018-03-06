51049fa2092f3a2431a1cf6e47372707180819a5
Marchogh <Marchogh@hotmail.com>
2018-03-06 13:35:55 +0100

    nasdadsa

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ $('a').click(function(){
   }, 500);
   return false;
 });
+
+$("#my-form").submit(function (e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function () {
+    alert("Thank you!");
+  });
+});
