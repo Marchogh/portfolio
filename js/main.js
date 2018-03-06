2adeaeee865b5013f5d35f5eee963325e23c5bdd
Marchogh <Marchogh@hotmail.com>
2018-03-06 13:23:33 +0100

    ajax form

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ $('a').click(function(){
   }, 500);
   return false;
 });
+
+
+$("#contact").submit(function (e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function () {
+    alert("Thank you!");
+  });
+});
